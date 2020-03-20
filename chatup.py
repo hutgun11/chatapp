125730c7c26b87d4ffe697d4e95e8222cfd70478
hutgun11@gmail.com <34814323+hutgun11@users.noreply.github.com>
2020-03-20 18:20:49 +0700

    cola

--- chatup.py
+++ chatup.py
@@ -1,14 +1,9 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-from flask_mysqldb import MySQL
-import psycopg2 as p
+
 
 app = Flask(__name__)
-# app.config['MYSQL_HOST']='34.87.185.36'
-# app.config['MYSQL_USER']='root'
-# app.config['MYSQL_PASSWORD']='018034538'
-# app.config['MYSQL_DB']='geography'
-# mysql= MySQL(app)
+
 app.config[ 'SECRET_KEY' ] = 'jsbcfsbfjefebw237u3gdbdc'
 socketio = SocketIO( app )
 
